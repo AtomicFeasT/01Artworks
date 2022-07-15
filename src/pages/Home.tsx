f0d7c20929c333e03fe51f95172a52ead791877b
AtomicFeasT <atomic-feast@hotmail.com>
2022-07-15 00:48:37 -0300

    Changed src/assets

---
+++ src/pages/Home.tsx
@@ -0,0 +1,299 @@
+export function Home() {
+    return (
+        <>
+        <header>
+        <h1>01 Artworks</h1> 
+        <p>A young and proud artist.</p>
+        </header>
+        
+        <main>
+        
+        <div className="section-title">
+        <h2>On Paper</h2>
+        </div>
+        
+        <section className="on-paper-regular">
+        {/* <!-- Example: width="300" height="300" --> */}
+        
+        {/* <!-- * First Row --> */}
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="regular-image-content" src="../images/regular/angel.jpg" alt="Angel" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Angel</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="regular-image-content" src="../images/regular/anxious.jpg" alt="Anxious" width="270" height="270" />
+        </div>
+        <div className="artwork-title">
+        <p>Anxious</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="regular-image-content" src="../images/regular/cheese-top-hat.jpg" alt="Cheese Top Hat" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Cheese Top Hat</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="regular-image-content" src="../images/regular/cute-cheesy-hair.jpg" alt="Cute Cheesy" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Cute Cheesy Hair</p>
+        </div>
+        
+        </div>
+        {/* <!-- ! Row Ending --> */}
+        
+        {/* <!-- * Second Row --> */}
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper rounded-frame">
+        {/* <img className="regular-image-content rounded-image" src="../images/regular/diana-fanart.jpg" alt="Diana Fanart" width="270" height="270"> */}
+        </div>
+        <div className="artwork-title">
+        <p>Diana Fanart</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper rounded-frame">
+        <img className="regular-image-content rounded-image" src="../images/regular/female-yasuo.jpg" alt="Female Yasuo" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Female Yasuo</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper rounded-frame">
+        <img className="regular-image-content rounded-image" src="../images/regular/xayah-chibi.jpg" alt="Xayah Chibi" width="270" height="270"/>
+        </div>
+        <div className="artwork-title"> 
+        <p>Xayah Chibi</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper rounded-frame">
+        <img className="regular-image-content rounded-image" src="../images/regular/hasegui.jpg" alt="Yasuo Chibi" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Yasuo Chibi</p>
+        </div>
+        
+        </div>
+        {/* <!-- ! Row Ending --> */}
+        
+        {/* <!-- * Third Row --> */}
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="regular-image-content" src="../images/regular/hand-a-rose-to-my-love.jpg" alt="Hand A Rose To My Love" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Hand A Rose To My Love</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="regular-image-content" src="../images/regular/happy-and-parrot.jpg" alt="Happy And Parrot" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Happy And Parrot</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="regular-image-content" src="../images/regular/inquisitive.jpg" alt="Inquisitive" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Inquisitive</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="regular-image-content" src="../images/regular/ready-for-war.jpg" alt="Ready For War" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Ready For War</p>
+        </div>
+        
+        </div>
+        {/* <!-- ! Row Ending --> */}
+        
+        {/* <!-- * Fourth Row --> */}
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper rounded-frame">
+        <img className="regular-image-content rounded-image" src="../images/regular/pizza.jpg" alt="Pizza!" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Pizza!</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper rounded-frame">
+        <img className="regular-image-content rounded-image" src="../images/regular/representation.jpg" alt="Representation" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Portrayal</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper rounded-frame">
+        <img className="regular-image-content rounded-image" src="../images/regular/surprised.jpg" alt="Surprised" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Surprised</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper rounded-frame">
+        <img className="regular-image-content rounded-image" src="../images/regular/gary.jpg" alt="Gary" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Gary</p>
+        </div>
+        
+        </div>
+        {/* <!-- ! Row Ending --> */}
+        
+        </section>
+        
+        <section className="on-paper-large">
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="large-image-content" src="../images/large/triple-horn.jpg" alt="Triple Horn" width="300" height="350"/>
+        </div>
+        <div className="artwork-title">
+        <p>Triple Horn</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="large-image-content" src="../images/large/triple-horn-side-profile.jpg" alt="Triple Horn Side Profile" width="300" height="350"/>
+        </div>
+        <div className="artwork-title">
+        <p>Triple Horn Side</p>
+        </div>
+        
+        </div>
+        </section>
+        
+        <section className="on-paper-medium">
+        {/* <!-- * First Row --> */}
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="medium-image-content" src="../images/medium/megalomaniac.jpg" alt="Megalomaniac" width="300" height="230"/>
+        </div>
+        <div className="artwork-title">
+        <p>Obsession</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="medium-image-content" src="../images/medium/taiga-fanart.jpg" alt="Taiga Fanart" width="300" height="230"/>
+        </div>
+        <div className="artwork-title">
+        <p>Taiga Fanart</p>
+        </div>
+        
+        </div>
+        {/* <!-- ! Row Ending --> */}
+        
+        </section>
+        
+        <div className="section-title">
+        <h2>Digitalized</h2>
+        </div>
+        
+        <section className="digitalized-regular">
+        {/* <!-- * First Row --> */}
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="regular-image-content" src="../images/regular/our-love.jpg" alt="Our Love" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Our Love</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="regular-image-content" src="../images/regular/diana-high-end.jpg" alt="Diana High End" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Diana High End</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="regular-image-content" src="../images/regular/tired-but-firm.jpg" alt="Tired But Firm" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Tired But Firm</p>
+        </div>
+        
+        </div>
+        <div className="artwork-wrapper">
+        
+        <div className="image-wrapper">
+        <img className="regular-image-content" src="../images/regular/fire-flex.jpg" alt="Fire Flex" width="270" height="270"/>
+        </div>
+        <div className="artwork-title">
+        <p>Fire Flex</p>
+        </div>
+        
+        </div>
+        {/* <!-- ! Row Ending --> */}
+        </section>
+        
+        </main>
+        
+        <footer>
+        <p>© Thiago Lisboa, Júlia Rossi</p>
+        </footer>
+        </>
+        
+        ) 
+        
+    }
