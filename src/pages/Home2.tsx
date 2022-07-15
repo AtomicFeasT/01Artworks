0186a966ef0e189c016768e60d79b28d4ad39391
AtomicFeasT <atomic-feast@hotmail.com>
2022-07-15 01:32:28 -0300

    Started developing the second page

---
+++ src/pages/Home2.tsx
@@ -0,0 +1,26 @@
+export function Home2() {
+    return (
+        <>
+        <nav className="nav-bar">
+            <menu>
+                <a href="#" className="button"></a>
+            </menu>
+            <div className="social-bar">
+                <a href="#" className="facebook button"></a>
+                <a href="#" className="twitter button"></a>
+                <a href="#" className="instagram button"></a>
+                <a href="#" className="pinterest button"></a>
+            </div>
+        </nav>
+        <main className="main">
+            <div className="hero-wrapper">
+                <img src="" alt="" className="hero-image" />
+            </div>
+            <div className="logo">
+                <p className="light-logo"></p>
+                <p className="strong-logo"></p>
+            </div>
+        </main>
+        </>
+    )
+}
