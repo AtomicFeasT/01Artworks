e78fcce0405a2befe80db8f1436868208b62bc9b
AtomicFeasT <atomic-feast@hotmail.com>
2022-07-21 18:13:11 -0300

    Fixed broken stuff and added refs

--- src/pages/Home2.tsx
+++ src/pages/Home2.tsx
@@ -1,24 +1,30 @@
+import { useRef } from "react"
 export function Home2() {
+    const home = useRef<HTMLDivElement>(null)
+    const hero = useRef<HTMLDivElement>(null)
+    const logoG = useRef<HTMLDivElement>(null)
+    const gallery = useRef<HTMLDivElement>(null)
+    const slide = useRef<HTMLDivElement>(null)
 
     function showGallery(argument: boolean) {
-        let home = document.querySelector<HTMLDivElement>('#homeSection')!
-        let hero = document.querySelector<HTMLDivElement>('#hero')!
-        let logoG = document.querySelector<HTMLDivElement>('#logoGallery')!
-        let gallery = document.querySelector<HTMLDivElement>('#gallerySection')!
-        let slide = document.querySelector<HTMLDivElement>('#slide')!
+        // let home = document.querySelector<HTMLDivElement>('#homeSection')!
+        // let hero = document.querySelector<HTMLDivElement>('#hero')!
+        // let logoG = document.querySelector<HTMLDivElement>('#logoGallery')!
+        // let gallery = document.querySelector<HTMLDivElement>('#gallerySection')!
+        // let slide = document.querySelector<HTMLDivElement>('#slide')!
         
         if (argument) {
-            home?.classList.add('hideHome')
-            hero?.classList.add('hideHero')
-            logoG?.classList.add('logoGalleryShow')
-            gallery?.classList.add('showGallery')
-            slide?.classList.add('showSlide')
+            home.current?.classList.add('hideHome')
+            hero.current?.classList.add('hideHero')
+            logoG.current?.classList.add('logoGalleryShow')
+            gallery.current?.classList.add('showGallery')
+            slide.current?.classList.add('showSlide')
         } else {
-            home?.classList.remove('hideHome')
-            hero?.classList.add('hideHero')
-            logoG?.classList.remove('logoGalleryShow')
-            gallery?.classList.remove('showGallery')
-            slide?.classList.remove('showSlide')
+            home.current?.classList.remove('hideHome')
+            hero.current?.classList.remove('hideHero')
+            logoG.current?.classList.remove('logoGalleryShow')
+            gallery.current?.classList.remove('showGallery')
+            slide.current?.classList.remove('showSlide')
         }
 
     }
@@ -26,15 +32,15 @@ export function Home2() {
     return (
         <>
         <main className="main"> 
-            <section className="main-section" id="homeSection">
-                <div className="hero-wrapper" id="hero">
+            <section className="main-section" ref={home} id="homeSection">
+                <div className="hero-wrapper" ref={hero} id="hero">
                     <img src="../../../public/pexels-medhat-ayad.jpg" alt="" />
                 </div>
 
                 <img src="../../../public/arrow-down-solid.svg" className="arrow-button" onClick={() => showGallery(true)}></img>
             </section>
 
-            <section className="gallery-section" id="gallerySection">
+            <section className="gallery-section" ref={gallery} id="gallerySection">
                 <div id="logoGallery">
                     <p className="strong-logo" onClick={() => showGallery(false)}></p>
                 </div>
