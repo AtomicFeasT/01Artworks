b8a2f258279e802bdafa9bc042911ed516bcdad6
AtomicFeasT <atomic-feast@hotmail.com>
2022-07-28 21:09:10 -0300

    Need to change to typescript

--- src/pages/HomeRevamp.tsx
+++ src/pages/HomeRevamp.tsx
@@ -24,11 +24,23 @@ const Section: FunctionComponent<SectionProps> = ({
 }
 
 // Carousel Logic
-// const buttons = document.querySelectorAll('[]')
+const buttons = document.querySelectorAll('[data-carousel-button]')
+const button = document.querySelector('button')
 
-// buttons.forEach(button => {
-    
-// });
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button.closest('[data-carousel]').querySelector('[data-slides]')
+
+        const activeSlide = slides?.querySelector('[data-active]')
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides?.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+    })
+}
 
 export function HomeRevamp() {
     const [showClass, setShowClass] = useState(false)
@@ -59,9 +71,10 @@ export function HomeRevamp() {
 
             <section className={`gallery-section ${showClass ? 'showGallery': ''}`} id="gallerySection">
 
-                <div className="slides-wrapper carousel">
+                <div data-carousel className="slides-wrapper carousel">
 
-                    <div className={`slide-show-wrapper ${showClass ? 'showSlides': ''}`}>
+                    <div data-slides className={`slide-show-wrapper ${showClass ? 'showSlides': ''}`}>
+
                         <div data-active className="slide-show first-slide">
                             <div className="modern-frame">
                                 <img className="frame-image" src="./assets/regular/anxious.jpg" alt="" />
@@ -109,8 +122,8 @@ export function HomeRevamp() {
                     </div>
              
                     <div className="buttons">
-                        <button><img className="button-left prev" src="./assets/arrows/circle-arrow-left-solid.svg" alt="" /></button>
-                        <button><img className="button-right next" src="./assets/arrows/circle-arrow-right-solid.svg" alt="" /></button>
+                        <button data-carousel-button="prev"><img className="button-left prev" src="./assets/arrows/circle-arrow-left-solid.svg" alt="" /></button>
+                        <button data-carousel-button="next"><img className="button-right next" src="./assets/arrows/circle-arrow-right-solid.svg" alt="" /></button>
                     </div>
                 </div>
               
